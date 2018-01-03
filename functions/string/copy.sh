e349826f35912655073e005c481b510ba7efc73f
Alex <alexzhangs@gmail.com>
2018-01-03 14:46:52 +0800

    Add functions/string/copy.sh

---
+++ functions/string/copy.sh
@@ -0,0 +1,17 @@
+# foo:desc:
+# Copy value of variable 1 (named by $1) to variable 2 (named by $2).
+# $1 can not be an Array.
+
+# foo:usage:
+# x=1
+# $foo x y
+#
+# x[1]=1
+# $foo x[1] y[1]
+
+function x-string-copy () {
+    [[ "$1" == "$2" ]] && return 9
+    unset $2  # in case $2 is an array
+    eval $2=\${$1}
+    return $?
+}
