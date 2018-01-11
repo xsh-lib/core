f8dbb600f8161ee73690090a3bd897f833f0b443
Alex <alexzhangs@gmail.com>
2018-01-11 17:55:39 +0800

    Draft of function/string

--- functions/string/repeat/2.sh
+++ functions/string/repeat/2.sh
@@ -16,17 +16,19 @@
 #?
 function repeat () {
     local str=$1 times=${2:-1}
+    local pmln i i_str
 
     if [[ -z ${str} ]]; then
         return
     fi
 
-    local pmln=$(( $(xmath.log_pmln "${#str}" 2 "${times}") + 2 )) || return 1
+    pmln=$(( $(xsh /math/log-pmln "${#str}" 2 "${times}") + 2 )) || return
 
-    local i=0 i_str=${str}
+    i=0
+    i_str=${str}
     while [[ ${i} -lt ${pmln} ]]; do
-        i_str=${i_str}${i_str} || return 1
-	    let i++
+        i_str=${i_str}${i_str} || return
+        let i++
     done
 
     echo "${i_str}" | cut -c1-$(( ${#str} * times ))
