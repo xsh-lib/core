ebc34f07cd1ecb048c49071e21de2985cd9e8b0e
Alex <alexzhangs@gmail.com>
2018-01-11 15:54:13 +0800

    Draft util selector

--- functions/string/repeat/4.sh
+++ functions/string/repeat/4.sh
@@ -1,1 +1,25 @@
-4.0.sh
+#? Usage:
+#?   @repeat STRING [N]
+#?
+#? Options:
+#?   STRING  String to repeat.
+#?   [N]     Repeat N times, default is 1, means no repeat.
+#?
+#? Output:
+#?   Concatenation of N STRINGs.
+#?
+#? Example:
+#?   @repeat Foo
+#?   Foo
+#?   @repeat Foo 3
+#?   FooFooFoo
+#?
+function repeat () {
+    local str=$1 times=${2:-1}
+
+    if [[ -z ${str} ]]; then
+        return
+    fi
+
+    jot -s "" -b "${str}" "${times}"
+}
