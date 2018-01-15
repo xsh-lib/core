2a402b9fc3ad8d2483499bece7c398e01e182ef9
Alex <alexzhangs@gmail.com>
2018-01-16 00:24:16 +0800

    Update string/repeat

---
+++ functions/string/repeat/6.sh
@@ -0,0 +1,25 @@
+#? Usage:
+#?   @repeat STRING [N]
+#?
+#? Options:
+#?   STRING  String to repeat.
+#?   [N]     Repeat N times, default is 1, means no repeat.
+#?
+#? Output:
+#?   Concatenation of N STRINGs.
+#?
+#? Example:
+#?   @repeat Foo
+#?   Foo
+#?   @repeat Foo 3
+#?   FooFooFoo
+#?
+function repeat () {
+    local str=$1 times=${2:-1}
+
+    if [[ -z ${str} ]]; then
+        return
+    fi
+
+    awk -v str=${str} -v times=${times} 'BEGIN {while (i++ < times) printf str}'
+}
