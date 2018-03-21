2feafe831eacc175d1234fed4cde7dc78e31e10b
Alex <alexzhangs@gmail.com>
2018-03-21 10:40:41 +0800

    Update file/abspath.sh: nice output for directory

--- functions/file/abspath.sh
+++ functions/file/abspath.sh
@@ -12,6 +12,12 @@
 #?   # /etc/passwd
 #?
 function abspath () {
+    local dir file
+    if [[ -z $1 ]]; then
+        printf "ERROR: parameter PATH null or not set.\n" >&2
+        return 255
+    fi
+
     if [[ ! -e $1 ]]; then
         printf "ERROR: PATH does not exist or permission denied.\n" >&2
         return 255
@@ -19,7 +25,17 @@ function abspath () {
 
     if xsh /file/is_abspath "$1"; then
         echo "$1"
+    elif [[ -d $1 ]]; then
+        echo "$(cd "$1" && pwd)"
     else
-        echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+        dir=$(cd "$(dirname "$1")" && pwd)
+        file=$(basename "$1")
+
+        if [[ -z ${dir} || -z ${file} ]]; then
+            printf "ERROR: failed to get dir or file part.\n" >&2
+            return 255
+        else
+            echo "${dir}/${file}"
+        fi
     fi
 }
