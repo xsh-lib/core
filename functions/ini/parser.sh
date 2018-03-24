a6700681ec12089d932ec48089f79d9498dc4575
Alex <alexzhangs@gmail.com>
2018-03-24 22:13:07 +0800

    Update ini/parser.sh to support -a, -s and -q

--- functions/ini/parser.sh
+++ functions/ini/parser.sh
@@ -1,14 +1,21 @@
 #? Usage:
-#?   @parser [-p PREFIX] INI_FILE
+#?   @parser [-a] [-p PREFIX] [-s] [-q] INI_FILE
 #?
 #? Options:
+#?   [-a]         Apply the environment variables.
+#?                With -a enabled, -s and -q are ignored.
+#?
 #?   [-p PREFIX]  Prefix variable name with PREFIX.
 #?                Default is '__INI_'.
+#?
+#?   [-s]         Enable signle mode, generate single expression for array assignment.
+#?
+#?   [-q]         Enable quotes, value will be quoted.
 #?
 #?   INI_FILE     Full path of INI file to parse.
 #?
 #? Output:
-#?   None.
+#?   Shell environment variables expression.
 #?
 #? Desription:
 #?   Parse an INI file into shell environment variables.
@@ -16,7 +23,7 @@
 #?
 #?   Below variables are set after INI file is parsed.
 #?     __INI_SECTIONS: Array, variable name suffix for all sections
-#?     __INI_SECTIONS_<section>: Value of <section>, without 
+#?     __INI_SECTIONS_<section>: Value of <section>, without
 #?     __INI_SECTIONS_<section>_KEYS: Array, variable name suffix of all keys in <section>
 #?     __INI_SECTIONS_<section>_KEYS_<key>: variable name suffix of <key> in <section>
 #?     __INI_SECTIONS_<section>_VALUES_<key>: Value of <key> in <section>
@@ -32,30 +39,42 @@
 #?     key4=4
 #?
 #?   @parser foo.ini
-#?   # Following variables were set:
-#?   # __INI_SECTIONS=([0]="section_b" [1]="section_a")
-#?   # __INI_SECTIONS_section_a='section a'
-#?   # __INI_SECTIONS_section_a_KEYS=([0]="key2" [1]="" [2]="key1")
+#?   # __INI_SECTIONS_section_a=section a
 #?   # __INI_SECTIONS_section_a_KEYS_key1=key1
-#?   # __INI_SECTIONS_section_a_KEYS_key2=key2
 #?   # __INI_SECTIONS_section_a_VALUES_key1=1
+#?   # __INI_SECTIONS_section_a_KEYS_key2=key2
 #?   # __INI_SECTIONS_section_a_VALUES_key2=2
-#?   # __INI_SECTIONS_section_b='section b'
-#?   # __INI_SECTIONS_section_b_KEYS=([0]="key4" [1]="key3")
+#?   # __INI_SECTIONS_section_a_KEYS[0]=key2
+#?   # __INI_SECTIONS_section_a_KEYS[1]=key1
+#?   # __INI_SECTIONS_section_b=section b
 #?   # __INI_SECTIONS_section_b_KEYS_key3=key3
-#?   # __INI_SECTIONS_section_b_KEYS_key4=key4
 #?   # __INI_SECTIONS_section_b_VALUES_key3=3
+#?   # __INI_SECTIONS_section_b_KEYS_key4=key4
 #?   # __INI_SECTIONS_section_b_VALUES_key4=4
+#?   # __INI_SECTIONS_section_b_KEYS[0]=key4
+#?   # __INI_SECTIONS_section_b_KEYS[1]=key3
+#?   # __INI_SECTIONS[0]=section_b
+#?   # __INI_SECTIONS[1]=section_a
 #?
 function parser () {
     local opt OPTIND OPTARG
-    local prefix ini_file
+    local apply prefix single quote ini_file
+    local ln
     local BASE_DIR="${XSH_HOME}/lib/x/functions/ini"  # TODO: use varaible instead
 
-    while getopts p: opt; do
+    while getopts ap:sq opt; do
         case ${opt} in
+            a)
+                apply=1
+                ;;
             p)
                 prefix=${OPTARG}
+                ;;
+            s)
+                single=1
+                ;;
+            q)
+                quote=1
                 ;;
             *)
                 return 255
@@ -70,9 +89,21 @@ function parser () {
         return 255
     fi
 
-    source /dev/stdin <<< "$(
-        awk -v prefix="${prefix:-__INI_}" \
+    prefix=${prefix:-__INI_}
+
+    if [[ ${apply} ]]; then
+        while read ln; do
+            xsh /string/global "${ln}"
+        done <<< "$(
+             awk -v prefix="${prefix}" \
+                 -f "${BASE_DIR}/parser.awk" \
+                 "${ini_file}"
+             )"
+    else
+        awk -v prefix="${prefix}" \
+            -v single="${single}" \
+            -v quote="${quote}" \
             -f "${BASE_DIR}/parser.awk" \
             "${ini_file}"
-    )"
+    fi
 }
