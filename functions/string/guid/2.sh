e1acfee0eb36318f1e0ee0c95747270901c7e55f
Alex <alexzhangs@gmail.com>
2019-07-26 06:37:55 +0800

    Update 2.sh
    fix defect: wrong length and no default length

--- functions/string/guid/2.sh
+++ functions/string/guid/2.sh
@@ -11,5 +11,5 @@
 #?   # db4eb095a0a921f0cc1804cf7d01fec7
 #?
 function guid () {
-    od  -vN "$1" -An -tx1 /dev/urandom | tr -d ' '
+    od -vN "$((${1:-32} / 2))" -An -tx1 /dev/urandom | tr -d ' '
 }
