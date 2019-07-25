2dea73db7935352c660f185cd00f1ec535797bb6
Alex <alexzhangs@gmail.com>
2019-07-26 05:53:13 +0800

    Update append.sh
    document the utility

--- functions/array/append.sh
+++ functions/array/append.sh
@@ -1,3 +1,17 @@
+#? Usage:
+#?   @append ARRAY VALUE ...
+#?
+#? Options:
+#?   ARRAY  Array name appending to.
+#?   VALUE  Value to append.
+#?
+#? Output:
+#?   Nothing.
+#?
+#? Example:
+#?   arr=([3]="III" [4]="IV"); @append arr V VI; declare -p arr
+#?   # declare -a arr='([3]="III" [4]="IV" [5]="V" [6]="VI")'
+#?
 function append () {
     local __i
 
