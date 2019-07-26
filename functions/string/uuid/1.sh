dfe805bef7cbaceff27e204d202daf27ec971ca4
Alex <alexzhangs@gmail.com>
2019-07-26 17:39:08 +0800

    create string/uuid
    the code is extracted from Gist

---
+++ functions/string/uuid/1.sh
@@ -0,0 +1,38 @@
+#? Description:
+#?   Generate a pseudo UUID
+#?
+#?   Author: markusfisch
+#?   Gist URL: https://gist.github.com/markusfisch/6110640
+#?
+#? Usage:
+#?   @uuid
+#?
+#? Output:
+#?   The UUID generated.
+#?
+#? Example:
+#?   @uuid
+#?   # 35b6aef6-af88-11e9-82fc-a3557caf0724
+#?
+function uuid () {
+    local N B C='89ab'
+    for (( N=0; N < 16; ++N ))
+    do
+        B=$(( $RANDOM%256 ))
+        case $N in
+            6)
+                printf '4%x' $(( B%16 ))
+                ;;
+            8)
+                printf '%c%x' ${C:$RANDOM%${#C}:1} $(( B%16 ))
+                ;;
+            3 | 5 | 7 | 9)
+                printf '%02x-' $B
+                ;;
+            *)
+                printf '%02x' $B
+                ;;
+        esac
+    done
+    echo
+}
