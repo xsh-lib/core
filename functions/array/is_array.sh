130d014a96a3bc3d01f63d20961a8b8885aab46f
Alex <alexzhangs@gmail.com>
2019-07-26 18:30:08 +0800

    Update is_array.sh
    add document

--- functions/array/is_array.sh
+++ functions/array/is_array.sh
@@ -1,3 +1,16 @@
+#? Usage:
+#?   @is_array VAR
+#?
+#? Options:
+#?   VAR  Variable name.
+#?
+#? Output:
+#?   Nothing.
+#?
+#? Example:
+#?   @is_array BASH_ARGV; echo $?
+#?   0
+#?
 function is_array () {
     [[ "$(declare -p "$1" 2>/dev/null)" =~ "declare -a" ]]
 }
