83ef582602982d3683a58dfca23316e30aa44c7c
Alex <alexzhangs@gmail.com>
2019-07-26 22:03:16 +0800

    rename test/runx.sh to test/rrun.sh

--- functions/test/rrun.sh
+++ functions/test/rrun.sh
@@ -1,28 +1,35 @@
+#? Description:
+#?   Repeatly run command for N times.
+#?
 #? Usage:
-#?   @runx [-s] N CMD [CMD_OPTIONS]
+#?   @rrun [-s] [-n TIMES] CMD [CMD_OPTIONS]
 #?
 #? Options:
 #?   [-s]           Shell mode, the command will be executed with eval.
-#?   N              How many times the command will be run. Default is 1.
+#?   [-n TIMES]     How many times the command will be run. Default is 1.
 #?   CMD            Command will be run.
 #?   [CMD_OPTIONS]  Command options, will be passed to command directly.
 #?
 #? Output:
+#?   Depends on the CMD.
 #?
 #? Example:
-#?   @runx 3 echo 'Hello World'
+#?   @rrun -n 3 echo 'Hello World'
 #?   Hello World
 #?   Hello World
 #?   Hello World
 #?
-function runx () {
+function rrun () {
     local OPTARG OPTIND opt
-    local shell i
-    
-    while getopts s opt; do
+    local shell times i
+
+    while getopts sn: opt; do
         case ${opt} in
             s)
                 shell=eval
+                ;;
+            n)
+                times=$OPTARG
                 ;;
             *)
                 return 255
@@ -31,7 +38,7 @@ function runx () {
     done
     shift $((OPTIND - 1))
 
-    for ((i=0; i<$1; i++)); do
+    for ((i=0; i<$times; i++)); do
         $shell "${@:2}"
     done
 }
