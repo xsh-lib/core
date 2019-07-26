4ab550e2597af975b122fc80499af301ea613e41
Alex <alexzhangs@gmail.com>
2019-07-26 23:10:19 +0800

    Update schema.sh
    remove 'file' schema since it's not in the standard of URI.
    add document.

--- functions/url/schema.sh
+++ functions/url/schema.sh
@@ -1,10 +1,29 @@
+#? Description:
+#?   Extract the schema part from an URI.
+#?
+#?   URI: https://en.wikipedia.org/wiki/Uniform_Resource_Identifier
+#?
+#? Usage:
+#?   @schema URI
+#?
+#? Options:
+#?   URI     The Uniform Resource Identifier (URI) will be used.
+#?
+#? Output:
+#?   The schema of the URI. 
+#?
+#? Example:
+#?   @schema https://github.com
+#?   # https
+#?
 function schema () {
     local schema
 
-    schema=${1%%://*}
+    schema=${1%%:*}
 
-    if [[ -z ${schema} || ${#schema} == ${#1} ]]; then
-        echo "file"
+    if [[ ${#schema} == ${#1} ]]; then
+        # No colon found, not an URI, so no schema
+        echo
     else
         xsh /string/lower "${schema}"
     fi
