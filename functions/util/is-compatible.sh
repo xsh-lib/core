80a3d7b02171aacd8dd2cf2208d686fa095a2e04
Alex <alexzhangs@gmail.com>
2019-07-28 16:09:21 +0800

    rename uitl/is_compitable.sh

---
+++ functions/util/is-compatible.sh
@@ -0,0 +1,20 @@
+#? Description:
+#?   Test whether a command is compatible with current environment.
+#?
+#? Usage:
+#?   @is-compatible CMD [OPTIONS]
+#?
+#? Example:
+#?   @is-compatible sed -r '' /dev/zero; echo $?
+#?   # 1
+#?
+#? Return:
+#?   0:     Compitable
+#?   != 0:  Not compatible
+#?
+#? Output:
+#?   Nothing.
+#?
+function is-compatible () {
+    "$@" >/dev/null 2>&1
+}
