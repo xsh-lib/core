1c430cff900f2e9a08a108b67282d8f7b43ab665
Alex <alexzhangs@gmail.com>
2019-07-29 23:36:21 +0800

    Update sql/Q/1.sh
    Leverage csv/parser to parse the table data rather than doing the parse from scrach.

--- functions/sql/Q/1.sh
+++ functions/sql/Q/1.sh
@@ -60,11 +60,11 @@
 #?     order by FIELD [asc | desc]
 #?
 function Q () {
-    xsh import /array/append /string/lower '/util/*' '/sql/*'
+    xsh import /array/append /string/lower '/util/*' '/sql/*' /csv/parser
 
     # Set default Field Separator (FS)
     local Q_FS=''     # Internal FS
-    local Q_IFS=$' '  # Input FS
+    local Q_IFS=$' '    # Input FS
     local Q_OFS=$'\t'   # Output FS
 
     local OPTIND OPTARG opt
@@ -116,7 +116,7 @@ function Q () {
         [6]="<="
     )
 
-    # main begin
+    # Parsing SQL
 
     x-sql-parser "$@" || return
 
@@ -124,66 +124,19 @@ function Q () {
         return 255
     fi
 
-    local tmp_table=/tmp/Q-$$-$RANDOM
+    # Parsing table data into array
 
-    # Remove the null lines
-    # Lines leading with '#' is commented
-    # Replace all Q_IFS to Q_FS, use \Q_IFS to escape a real Q_IFS, exam: \| is a character, not a delimiter
-    # Replace all \Q_IFS to Q_IFS after last step
-    # Remove the leading and trailing blank space for each line
-    # Remove the leading and trailing blank space for each column
-
-    awk NF "${Q_TABLE}" \
-        | awk '!/^#/' \
-        | x-util-sed-regex "s/([^\\])\\${Q_IFS}/\1${Q_FS}/g" \
-        | sed "s/[\\]${Q_IFS}/${Q_IFS}/g" \
-        | x-util-sed-regex 's/^[ \t]+|[ \t]+$//g' \
-        | x-util-sed-regex "s/[ \t]*($Q_FS)[ \t]*/\1/g" \
-                           > $tmp_table
-
-    row_count=$(awk 'END {printf NR - 1}' $tmp_table)
-
-    # Parsing table columns name into array
-    local tab_fields
-    IFS=$Q_FS read -r -a tab_fields <<< "$(head -1 $tmp_table)"
-
-    local tab_field_var_prefix='rows_of_'
-
-    # Initiate all table field variables
-    local tf_name
-    for tf_name in ${tab_fields[@]}; do
-        declare -a "$tab_field_var_prefix$tf_name"
-    done
-
-    # Populate all the values of table column into an array named as
-    # <tab_field_var_prefix><field_name>, column name is at index 0
-    # of each array.
-    local i=1 ln
-    for tf_name in ${tab_fields[@]}; do
-        while read ln; do
-            x-array-append "$tab_field_var_prefix$tf_name" "$ln"
-        done <<< "$(cut -d "$Q_FS" -f$i $tmp_table)"
-        let i++
-    done
-    /bin/rm -f "$tmp_table"
-
-    local query_field_var_prefix='q_rows_of_'
-
-    # Initiate all query field variables
-    local qf_name
-    for qf_name in "${Q_FIELDS[@]}"; do
-        declare -a "$query_field_var_prefix$qf_name"
-    done
+    x-csv-parser -I "$Q_IFS" -e -a -p 'Q_' "$Q_TABLE"
 
     # Process predcates
-    local candidate_row_indeces
+    declare -a candidate_row_indeces
 
     # Select all rows
     if [[ $header -eq 0 ]]; then
         # Surprise table header
-        candidate_row_indeces=( $(seq 1 $row_count) )
+        candidate_row_indeces=( $(seq 1 "$((Q_NR-1))") )
     else
-        candidate_row_indeces=( $(seq 0 $row_count) )
+        candidate_row_indeces=( $(seq 0 "$((Q_NR-1))") )
     fi
 
     Q_ROW_COUNT=${#candidate_row_indeces[@]}
@@ -197,11 +150,11 @@ function Q () {
     local row_index
     for row_index in ${candidate_row_indeces[@]}; do
         local i=0 qf_name ln
-        for qf_name in ${Q_FIELDS[@]}; do
+        for qf_name in "${Q_SELECTED_FIELDS[@]}"; do
             if [[ $i -gt 0 ]]; then
                 printf "%s" "$Q_OFS"
             fi
-            local varname="${tab_field_var_prefix}${qf_name}[$row_index]"
+            local varname="Q_FIELDS_${qf_name}_ROWS[$row_index]"
             printf "%s" "${!varname}"
             i=$((i+1))
         done
