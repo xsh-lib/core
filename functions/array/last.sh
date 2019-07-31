1f26d47ce2631b72d5181aa3e1eb5f4a84c52ac8
Alex <alexzhangs@gmail.com>
2019-08-01 04:33:44 +0800

    Add a new version of array/last.sh and set as default
    Use indirect expansion and the (-1) index to get last element directly

--- functions/array/last.sh
+++ functions/array/last.sh
@@ -1,18 +1,1 @@
-#? Usage:
-#?   @last ARRAY
-#?
-#? Options:
-#?   ARRAY  Array name.
-#?
-#? Output:
-#?   The last element in the array.
-#?
-#? Example:
-#?   arr=([3]="III" [4]="IV"); @last arr
-#?   # IV
-#?
-function last () {
-    # try to declare nothing, new variable may override input variable.
-    set -- "$1[$(xsh /array/ilast "$1")]"
-    echo "${!1}"
-}
+last/2.sh
---
+++ functions/array/last/2.sh
@@ -0,0 +1,18 @@
+#? Usage:
+#?   @last ARRAY
+#?
+#? Options:
+#?   ARRAY  Array name.
+#?
+#? Output:
+#?   The last element in the array.
+#?
+#? Example:
+#?   arr=([3]="III" [4]="IV"); @last arr
+#?   # IV
+#?
+function last () {
+    # try to declare nothing, new variable may override input variable.
+    set -- "$1[@]"
+    echo "${!1:(-1)}"
+}
