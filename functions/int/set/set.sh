08e94da81448cd1f441f26409030ff7f2011aa1f
Alex <alexzhangs@gmail.com>
2019-07-31 21:35:38 +0800

    Create int/set/set.sh

---
+++ functions/int/set/set.sh
@@ -0,0 +1,42 @@
+#? Description:
+#?   Apply operand on 2 sorted sets.
+#?
+#? Usage:
+#?   @set SET OPERAND SET
+#?
+#? Options:
+#?   SET
+#?
+#?   The set is a string contains sorted elements delimited by whitespace or newline.
+#?
+#?   OPERAND
+#?
+#?   &
+#?   |
+#?
+#? Exmaple:
+#?   @set '1 2 3 4' \& '3 4 5 6'
+#?   # 3
+#?   # 4
+#?
+function set () {
+
+    function pipe_space_to_newline () {
+        awk '{for(i=1;i<=NF;i++) {print $i}}'
+    }
+
+    case $2 in
+        \&)
+            join <(echo "$1" | pipe_space_to_newline) \
+                 <(echo "$3" | pipe_space_to_newline)
+            ;;
+        \|)
+            join -a 1 -a 2 \
+                 <(echo "$1" | pipe_space_to_newline) \
+                 <(echo "$3" | pipe_space_to_newline)
+            ;;
+        *)
+            return 255
+            ;;
+    esac
+}
