189e9676efdec18f20ae637a3bc4ba69456947a3
Alex <alexzhangs@gmail.com>
2019-08-01 22:17:32 +0800

    Update sql/Q/1.sh
    Add logic to handle where clause.
    Update document.

--- functions/sql/Q/1.sh
+++ functions/sql/Q/1.sh
@@ -5,22 +5,6 @@
 #?   the field delimiter for output is '\t'.
 #?   The row delimiter is new line for both input and output.
 #?
-#? Return:
-#?   0: succeed
-#?   255: error
-#?   100: 0 row selected
-#?
-#? Export:
-#?   Q_TABLE: always set
-#?   Q_FIELDS: always set
-#?   Q_ROW_COUNT: always set
-#?
-#? Output:
-#?   Query result.
-#?
-#? Examples:
-#?   @Q select f1 f2 f3 from A
-#?
 #? Usage:
 #?   @Q [OPTIONS] SELECT-CLAUSE FROM-CLAUSE \
 #?       [WHERE-CLAUSE]
@@ -45,19 +29,28 @@
 #?
 #?     Show the table header in the reuslt output if specified.
 #?
-#?   SELECT-CLAUSE
-#?     [distinct] [*] [FIELDS]
+#? Return:
+#?   0: succeed
+#?   255: error
+#?   100: 0 row selected
 #?
-#?   FROM-CLAUSE
-#?     from TABLE-NAME
+#? Export:
+#?   Q_ROW_COUNT: always set
+#?   Q_FIELDS_*:  always set
 #?
-#?   [WHERE-CLAUSE]
-#?     where SEARCH-CONDITION
+#? Output:
+#?   Query result without header.
 #?
-#?     SEARCH-CONDITION
+#? Examples:
+#?   cat A
+#?   # a b c
+#?   # 1 4 7
+#?   # 2 5 8
+#?   # 3 6 9
 #?
-#?   [ORDER-BY-CLAUSE]
-#?     order by FIELD [asc | desc]
+#?   @Q select a,b,c from A where a = 1 or b = 5
+#?   # 1	4	7
+#?   # 2	5	8
 #?
 function Q () {
     xsh import /array/append /string/lower '/util/*' '/sql/*' /csv/parser
@@ -95,29 +88,28 @@ function Q () {
 
     RESERVED_KEYWORDS=(
         "select"
-        #"distinct"
         "from"
-        #"where"
-        #"and"
-        #"or"
-        #"order"
-        #"by"
-        #"asc"
-        #"desc"
+        "where"
+        "and"
+        "or"
     )
 
     OPERATORS=(
-        [0]="like"
         [1]="="
         [2]="!="
         [3]=">"
         [4]="<"
-        [5]=">="
-        [6]="<="
+        [5]="-eq"
+        [6]="-ne"
+        [7]="-gt"
+        [8]="-ge"
+        [9]="-lt"
+        [10]="-le"
     )
 
     # Parsing SQL
 
+    local Q_SELECTED_FIELDS Q_TABLE Q_WHERE
     x-sql-parser "$@" || return
 
     if [[ ! -f $Q_TABLE ]]; then
@@ -126,29 +118,85 @@ function Q () {
 
     # Parsing table data into array
 
+    local Q_FIELDS Q_NR
     x-csv-parser -I "$Q_IFS" -e -a -p 'Q_' "$Q_TABLE"
 
-    # Process predcates
-    declare -a candidate_row_indeces
+    # Process where clause
 
-    # Select all rows
-    if [[ $header -eq 0 ]]; then
-        # Surprise table header
-        candidate_row_indeces=( $(seq 1 "$((Q_NR-1))") )
+    local selected_row_indeces
+
+    if [[ ${#Q_WHERE[@]} -gt 0 ]]; then
+
+        # Process predicates
+
+        declare -a candidate_set
+
+        # Always select header
+        candidate_set[0]=0
+        candidate_set[1]='|'
+
+        local s_field  # search field
+        local s_operator  # search operator
+
+        local i=1 expr
+        for expr in "${Q_WHERE[@]}"; do
+            case $expr in
+                '('|')')
+                    candidate_set[${#candidate_set[@]}]=$expr
+                    ;;
+                *)
+                    case $((i % 4)) in
+                        1)  # key
+                            s_field=$expr
+                            ;;
+                        2)  # operator
+                            s_operator=$expr
+                            ;;
+                        3)  # value
+                            candidate_set[${#candidate_set[@]}]="$(
+                                xsh /array/search -o "$s_operator" "Q_FIELDS_${s_field}_ROWS" "$expr")"
+                            ;;
+                        0)  # and/or
+                            case $expr in
+                                and)
+                                    candidate_set[${#candidate_set[@]}]='&'
+                                    ;;
+                                or)
+                                    candidate_set[${#candidate_set[@]}]='|'
+                                    ;;
+                            esac
+                            ;;
+                    esac
+                    i=$((i + 1))
+                    ;;
+            esac
+        done
+
+        # Calculate candidate sets expression
+        selected_row_indeces=( $(xsh /int/set/eval "${candidate_set[@]}") )
     else
-        candidate_row_indeces=( $(seq 0 "$((Q_NR-1))") )
+        selected_row_indeces=( $(seq 0 "$((Q_NR - 1))") )
     fi
 
-    Q_ROW_COUNT=${#candidate_row_indeces[@]}
+    # Process table header
 
+    if [[ $header -eq 0 ]]; then
+        # Suppress table header
+        unset selected_row_indeces[0]
+    fi
+
+    # Process row count
+
+    Q_ROW_COUNT=$((${#selected_row_indeces[@]} - 1))
     if [[ $Q_ROW_COUNT -eq 0 ]]; then
         # No rows returned
         return 100
     fi
 
-    # Build record set
+    # Build result record set
+
     local row_index
-    for row_index in ${candidate_row_indeces[@]}; do
+    for row_index in ${selected_row_indeces[@]}; do
         local i=0 qf_name ln
         for qf_name in "${Q_SELECTED_FIELDS[@]}"; do
             if [[ $i -gt 0 ]]; then
@@ -156,10 +204,10 @@ function Q () {
             fi
             local varname="Q_FIELDS_${qf_name}_ROWS[$row_index]"
             printf "%s" "${!varname}"
-            i=$((i+1))
+            i=$((i + 1))
         done
         echo
     done
 
-    return 0
+    return
 }
