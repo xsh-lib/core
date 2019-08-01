dcf59f98662224b000a36c892515fe2607c2c0fb
Alex <alexzhangs@gmail.com>
2019-08-01 12:12:24 +0800

    Create int/set/op-comparator.sh

---
+++ functions/int/set/op-comparator.sh
@@ -0,0 +1,52 @@
+#? Description:
+#?   Operators comparator, return the priority of 2 operators.
+#?
+#? Usage:
+#?   @op-comparator [OPERATOR] [OPERATOR]
+#?
+#? Output:
+#?   = 0: The two have equal priority.
+#?   > 0: The former is higher than the later.
+#?   < 0: The former is lower than the later.
+#?
+#? Example:
+#?   @op-comparator '&' '|'
+#?   # 1
+#?
+function op-comparator () {
+
+    local OPERATORS=(
+        [0]='|'
+        [1]='&'
+    )
+
+    function priority () {
+        case $1 in
+            '|')
+                echo 1
+                ;;
+            '&')
+                echo 2
+                ;;
+            *)
+                return 255
+                ;;
+        esac
+    }
+
+    local p1 p2 ret
+    if [[ -n $1 && -n $2 ]]; then
+        p1=$(priority "$1")
+        p2=$(priority "$2")
+        if [[ -n $p1 && -n $p2 ]]; then
+            ret="$((p1 - p2))"
+        else
+            ret=
+        fi
+    else
+        ret="${OPERATORS[*]}"
+    fi
+
+    unset priority
+    echo ${ret:?}
+}
