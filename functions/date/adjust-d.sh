1498bfb302a705d52561bfa967d41b4d16877007
Alex <alexzhangs@gmail.com>
2019-09-02 01:05:42 +0800

    Update adjust-d.sh

--- functions/date/adjust-d.sh
+++ functions/date/adjust-d.sh
@@ -39,9 +39,20 @@
 #?   [TIMESTAMP]  Base timestamp. If omitted current datetime is used.
 #?
 #? Bugs:
-#?   If `[+-]` in the option is unset, it won't act like `/date/adjust-v`, an error
-#?   will be returned.
-#?   This is decided by the `GNU date` itself.
+#?
+#?   1. `@adjust-d +1d Mon`
+#?      Avoid to use single weekday as the optional [TIMESTAMP], it will be recognized
+#?      as adjustment option of unsigned weekday.
+#?
+#?   2. `@adjust-d 100y`
+#?      Use unsigned year adjustment option with caution.
+#?      Unlike `BSD date`, @adjust-d won't check the valid of the setting year, and
+#?      won't handle following mapping:
+#?
+#?      * `2000: 00-68`
+#?      * `1900: 69-99`
+#?      * `1900: 100-1900`
+#?      * `0000: 1901-`
 #?
 #? Example:
 #?   @adjust-d 2008-10-10 21 day
@@ -78,46 +89,63 @@ function adjust-d () {
     #?
     #?    SOLUTION:
     #?
-    #?    * TODO
+    #?    * Use user-defined function to handle the logic.
+    #?
+    #? 3. Equivalent of BSD date `date -v <NUM>[ymdwHMS]`
+    #?
+    #?    * GNU date doesn't support to set a specific part of date.
+    #?
+    #?    SOLUTION:
+    #?
+    #?    * Use user-defined function to handle the logic.
     #?
 
     #? test whether the argument is for adjustment
     function __is_adjust_opt__ () {
-        egrep -q '^[+-][0-9]{1,}[ymdwHMS]$|^[+-][a-zA-Z]{3,}$' <<< "$(echo "$1")"
+        egrep -q '^[+-]?[0-9]{1,}[ymdwHMS]$|^[+-]?[a-zA-Z]{3,}$' <<< "$(echo "$1")"
     }
 
     #? test whether it's running into the highlight point 2
     function __has_weekday_opt__ () {
-        egrep -q '[+-][a-zA-Z]{3,}' <<< "$(echo "$*")"
+        egrep -q '[+-]?[a-zA-Z]{3,}' <<< "$(echo "$*")"
     }
 
-    #? translate short names to long names
-    function __short_to_long__ () {
-        case $1 in
-            y) echo year;;
-            m) echo month;;
-            d) echo day;;
-            w) echo week;;
-            H) echo hour;;
-            M) echo minute;;
-            S) echo second;;
-            *) return 255;;
-        esac
+    #? test whether it's running into the highlight point 3
+    function __has_unsigned_opt__ () {
+        egrep -q '(^| )[0-9]{1,}[a-zA-Z]' <<< "$(echo "$*")"
     }
 
     # translate BSD date style `-v` options to GNU date style `-d` options
     function __bsd_to_gnu__ () {
+
+        #? translate short names to long names
+        function __short_to_long__ () {
+            case $1 in
+                y) echo year;;
+                m) echo month;;
+                d) echo day;;
+                w) echo week;;
+                H) echo hour;;
+                M) echo minute;;
+                S) echo second;;
+                *) return 255;;
+            esac
+        }
+
         local sign digi unit
 
         sign=${1:0:1}          # get first char
         digi=${1//[^0-9]/}     # remove non-digit
         unit=${1//[^a-zA-Z]/}  # remove non-letter
 
-        local name
+        local name weekday
         case ${#unit} in
             1)
                 # [ymdwHMS]
                 name=$(__short_to_long__ "${unit}")
+
+                # clean env
+                unset -f __short_to_long__
 
                 if [[ -z $name ]]; then
                     printf "$FUNCNAME: ERROR: Invalid option: '%s' in '%s'.\n" "$unit" "$1" >&2
@@ -143,14 +171,16 @@ function adjust-d () {
         case $sign in
             +)
                 if [[ -n $weekday ]]; then
-                    prefix='next'
+                    echo "next $weekday"
+                else
+                    echo "$digi $name"
                 fi
                 ;;
             -)
                 if [[ -n $weekday ]]; then
-                    prefix='last'
+                    echo "last $weekday"
                 else
-                    suffix=' ago'
+                    echo "$digi $name ago"
                 fi
                 ;;
             *)
@@ -158,53 +188,6 @@ function adjust-d () {
                 return 255
                 ;;
         esac
-
-        echo "${prefix}${digi} ${name}${weekday}${suffix}"
-    }
-
-    #? Calculate the delta days between the base weekday and the target weekday.
-    #?
-    #? Usage:
-    #?   __calc_delta_weekday__ <BASE> <TARGET>
-    #?
-    #? Options:
-    #?   <BASE>    Base weekday: [1-7]
-    #?
-    #?   <TARGET>  Target weekday: [+-][1-7]
-    #?             With leading [+-], the target weekday is the next coming
-    #?             weekday or the weekday in the past.
-    #?             Without leading [+-], the target weekday is the weekday
-    #?             of this week.
-    #?
-    #? Output:
-    #?   -13 ~ +13
-    function __calc_delta_weekday__ () {
-        local base=${1:?}
-        local target=${2:?}
-
-        local sign_of_target=${target:0:1}   # get the plus or minus sign
-
-        local delta
-        case $sign_of_target in
-            [+-])
-                delta=$(( (target - ${sign_of_target}base + 7) % 7 ))
-                if [[ $delta -eq 0 ]]; then
-                    delta=7
-                fi
-                delta=${sign_of_target}${delta}
-                ;;
-            [0-9])
-                delta=$(( target - base ))
-                if [[ $delta -ge 0 ]]; thne
-                   delta=+$delta
-                fi
-                ;;
-            *)
-                return 255
-                ;;
-        esac
-
-        echo "$delta"
     }
 
     function __adjust-d__ () {
@@ -234,10 +217,55 @@ function adjust-d () {
         fi
     }
 
-    function __adjust-d-recursive__ () {
+    function __adjust-d-signle-step__ () {
+
+        #? Calculate the delta days between the base weekday and the target weekday.
+        #?
+        #? Usage:
+        #?   __calc_delta_weekday__ <BASE> <TARGET>
+        #?
+        #? Options:
+        #?   <BASE>    Base weekday: [1-7]
+        #?
+        #?   <TARGET>  Target weekday: [+-][1-7]
+        #?             With leading [+-], the target weekday is the next coming
+        #?             weekday or the weekday in the past.
+        #?             Without leading [+-], the target weekday is the weekday
+        #?             of this week.
+        #?
+        #? Output:
+        #?   -13 ~ +13
+        function __calc_delta_weekday__ () {
+            local base=${1:?}
+            local target=${2:?}
+
+            local sign_of_target=${target//[^+-]/}  # remove none [+-]
+
+            local delta
+            case $sign_of_target in
+                [+-])
+                    delta=$(( (target - ${sign_of_target}base + 7) % 7 ))
+                    if [[ $delta -eq 0 ]]; then
+                        delta=7
+                    fi
+                    delta=${sign_of_target}${delta}
+                    ;;
+                *)
+                    delta=$((target - base))
+                    if [[ $delta -ge 0 ]]; then
+                       delta=+$delta
+                    fi
+                    ;;
+            esac
+
+            echo "$delta"
+        }
+
         local ts
 
-        if ! __is_adjust_opt__ "${@:(-1)}"; then
+        if __is_adjust_opt__ "${@:(-1)}"; then
+            ts=$(date "${XSH_X_DATE__DATETIME_FMT:?}")
+        else
             # get last argument
             ts=${@:(-1)}
 
@@ -245,35 +273,58 @@ function adjust-d () {
             set -- "${@:1:$(($# - 1))}"
         fi
 
-        local adjust=$(__bsd_to_gnu__ "$1")
         local result
 
-        case ${adjust%% *} in
-            last|next)
-                local current=$(date -d "${ts}" +%u)  # 1 ~ 7
-                local target=${1:0:1}$(date -d "${1:1}" +%u)  # -7 ~ +7
+        local sign=${1//[^+-]/}  # remove none [+-]
+        local unit=${1//[^a-zA-Z]/}  # remove non-letter
 
-                local delta=$(__calc_delta_weekday__ "${current:?}" "${target:?}")
-                result=$(__adjust-d__ "${delta}d" "${ts}")
-                ;;
-            *)
-                local fmt=$(xsh /date/parser "${ts}")
-                result="$(date -d "${ts} $adjust" "+${fmt}")"
-                ;;
-        esac
+        if __has_weekday_opt__ "$1"; then
+            local current=$(date -d "$ts" +%u)  # 1 ~ 7
+            local target=$(date -d "$unit" +%u)  # 1 ~ 7
+
+            local delta=$(__calc_delta_weekday__ "${current:?}" "$sign${target:?}")
+            result=$(__adjust-d__ "${delta}d" "$ts")
+        else
+            case $sign in
+                [+-])
+                    local adjust=$(__bsd_to_gnu__ "$1")
+                    local fmt=$(xsh /date/parser "$ts")
+                    result="$(date -d "$ts $adjust" "+${fmt}")"
+                    ;;
+                *)
+                    local digi=${1//[^0-9]/}  # remove non-digit
+
+                    # hyphen `-`: don't pad the field
+                    # lower `y` to upper `Y`: get year with Century
+                    local current=$(date -d "$ts" +%-${unit/y/Y})
+
+                    local delta=$((digi - current))
+                    if [[ $delta -ge 0 ]]; then
+                        delta=+$delta
+                    fi
+
+                    result=$(__adjust-d__ "$delta$unit" "$ts")
+                    ;;
+            esac
+        fi
 
         shift
         if [[ $# -gt 0 ]]; then
-            # Call recursively to get result
-            result="$(__adjust-d-recursive__ "$@" "$result")"
+            # Call signle-steply to get result
+            result="$(__adjust-d-signle-step__ "$@" "$result")"
         fi
+
+        # clean env
+        unset -f __calc_delta_weekday__
 
         echo "$result"
     }
 
 
-    if ! __is_adjust_opt__ "${@:(-1)}" && __has_weekday_opt__ "$*"; then
-        __adjust-d-recursive__ "$@"
+    if __has_weekday_opt__ "$*" || __has_unsigned_opt__ "$*"; then
+        # handle logic of highlight point 2 and 3
+        __adjust-d-signle-step__ "$@"
+
     else
         __adjust-d__ "$@"
     fi
@@ -282,8 +333,8 @@ function adjust-d () {
     unset -f \
           __is_adjust_opt__ \
           __has_weekday_opt__ \
-          __short_to_long__ \
+          __has_unsigned_opt__ \
           __bsd_to_gnu__ \
           __adjust-d__ \
-          __adjust-d-recursive__
+          __adjust-d-signle-step__
 }
