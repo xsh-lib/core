9e3a905a0608ea4afa286c901dfcddbc7a289548
Alex <alexzhangs@gmail.com>
2019-09-04 22:36:17 +0800

    update doc of date/regulate.sh

--- functions/date/regulate.sh
+++ functions/date/regulate.sh
@@ -4,6 +4,13 @@
 #? Usage:
 #?   @regulate <TIMESTAMP>
 #?
+#? Example:
+#?   $ @regulate 'Feb 29 1900'
+#?   Mar 01 1900
+#?
+#?   $ @regulate 2012-1-1
+#?   2012-01-01
+#?
 function regulate () {
     local ts=$(xsh /string/trim "${1:?}")
     local fmt=$(xsh /date/parser "$ts")
