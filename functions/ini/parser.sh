30240093371650f434d55df567343cdbf96e87b2
Alex <alexzhangs@gmail.com>
2019-10-09 19:42:51 +0800

    update doc: ini/parser.sh

--- functions/ini/parser.sh
+++ functions/ini/parser.sh
@@ -1,60 +1,60 @@
-#? Usage:
-#?   @parser [-a] [-p PREFIX] [-q] [-s] INI_FILE
-#?
-#? Options:
-#?   [-a]         Apply the environment variables.
-#?                With -a enabled, -s and -q are ignored.
-#?
-#?   [-p PREFIX]  Prefix variable name with PREFIX.
-#?                Default is '__INI_'.
-#?
-#?   [-q]         Enable quotes, value will be quoted.
-#?
-#?   [-s]         Enable signle mode, generate single expression for array assignment.
-#?
-#?   INI_FILE     Full path of INI file to parse.
-#?
-#? Output:
-#?   Shell environment variables expression.
-#?
 #? Desription:
 #?   Parse an INI file into shell environment variables.
-#?   Link: https://en.wikipedia.org/wiki/INI_file
 #?
-#?   Below variables are set after INI file is parsed.
+#?   If set -a, below variables are set after INI file is parsed.
 #?     __INI_SECTIONS: Array, variable name suffix for all sections
 #?     __INI_SECTIONS_<section>: Value of <section>, without
 #?     __INI_SECTIONS_<section>_KEYS: Array, variable name suffix of all keys in <section>
 #?     __INI_SECTIONS_<section>_KEYS_<key>: variable name suffix of <key> in <section>
 #?     __INI_SECTIONS_<section>_VALUES_<key>: Value of <key> in <section>
 #?
+#? Usage:
+#?   @parser [-a] [-p PREFIX] [-q] [-s] INI_FILE
+#?
+#? Options:
+#?   [-a]         Apply the environment variables.
+#?                With -a enabled, output is turned off, and -s and -q are ignored.
+#?
+#?   [-p PREFIX]  Prefix variable name with PREFIX.
+#?                Default is '__INI_'.
+#?
+#?   [-q]         Enable quotes in output, value will be quoted.
+#?
+#?   [-s]         Enable signle mode in output, generate single expression for array
+#?                assignment.
+#?
+#?   INI_FILE     Full path of INI file to parse.
+#?
+#? Output:
+#?   Shell environment variables expression.
+#?
+#? Standard:
+#?   https://en.wikipedia.org/wiki/INI_file
+#?
 #? Example:
-#?   foo.ini
-#?     [section a]
-#?     key1=1
-#?     key2=2
+#?   $ cat foo.ini
+#?   [section a]
+#?   key1=1
+#?   key2=2
 #?
-#?     [section b]
-#?     key3=3
-#?     key4=4
+#?   [section b]
+#?   key3=3
+#?   key4=4
 #?
-#?   @parser foo.ini
-#?   # SECTIONS_section_a=section a
-#?   # SECTIONS_section_a_KEYS_key1=key1
-#?   # SECTIONS_section_a_VALUES_key1=1
-#?   # SECTIONS_section_a_KEYS_key2=key2
-#?   # SECTIONS_section_a_VALUES_key2=2
-#?   # SECTIONS_section_a_KEYS[0]=key1
-#?   # SECTIONS_section_a_KEYS[1]=key2
-#?   # SECTIONS_section_b=section b
-#?   # SECTIONS_section_b_KEYS_key3=key3
-#?   # SECTIONS_section_b_VALUES_key3=3
-#?   # SECTIONS_section_b_KEYS_key4=key4
-#?   # SECTIONS_section_b_VALUES_key4=4
-#?   # SECTIONS_section_b_KEYS[0]=key3
-#?   # SECTIONS_section_b_KEYS[1]=key4
-#?   # SECTIONS[0]=section_a
-#?   # SECTIONS[1]=section_b
+#?   $ @parser -a foo.ini; set | grep ^__INI_
+#?   __INI_SECTIONS=([0]="section_a" [1]="section_b")
+#?   __INI_SECTIONS_section_a='section a'
+#?   __INI_SECTIONS_section_a_KEYS=([0]="key1" [1]="key2")
+#?   __INI_SECTIONS_section_a_KEYS_key1=key1
+#?   __INI_SECTIONS_section_a_KEYS_key2=key2
+#?   __INI_SECTIONS_section_a_VALUES_key1=1
+#?   __INI_SECTIONS_section_a_VALUES_key2=2
+#?   __INI_SECTIONS_section_b='section b'
+#?   __INI_SECTIONS_section_b_KEYS=([0]="key3" [1]="key4")
+#?   __INI_SECTIONS_section_b_KEYS_key3=key3
+#?   __INI_SECTIONS_section_b_KEYS_key4=key4
+#?   __INI_SECTIONS_section_b_VALUES_key3=3
+#?   __INI_SECTIONS_section_b_VALUES_key4=4
 #?
 function parser () {
     local opt OPTIND OPTARG
