cf1ffdaf489d9682ac3efab07f0d31f85d1b5d8a
Alex <alexzhangs@gmail.com>
2019-10-09 15:11:42 +0800

    Create util/__init__.sh

---
+++ functions/util/__init__.sh
@@ -0,0 +1,9 @@
+#? -----------------------------------------------------------------------------
+#? xsh library INIT file.
+#?
+#? This file is sourced while importing any function utility, right before the
+#? function utility was sourced.
+#? -----------------------------------------------------------------------------
+
+
+xsh import /util
