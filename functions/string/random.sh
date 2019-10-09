fdac5045d1399e44cb63b3aaeed4a21c132d94d0
Alex <alexzhangs@gmail.com>
2019-10-09 22:08:44 +0800

    refactor symbol link

--- functions/string/random.sh
+++ functions/string/random.sh
@@ -1,1 +1,18 @@
-random/1.sh
+#? Usage:
+#?   @random [LENGTH]
+#?
+#? Options:
+#?   [LENGTH]  Number of characters to generate, default is 16.
+#?             LENGTH should be an positive even number, if it's an odd number
+#?             then will be rounded to an nearest smaller even number.
+#?
+#? Output:
+#?   The random string generated.
+#?
+#? Example:
+#?   $ @random 32
+#?   db4eb095a0a921f0cc1804cf7d01fec7
+#?
+function random () {
+    od -vN "$((${1:-16} / 2))" -An -tx1 /dev/urandom | tr -d ' '
+}
