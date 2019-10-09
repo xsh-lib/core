1889300b3d9e9a395309a83d7719548fb84dfb09
Alex <alexzhangs@gmail.com>
2019-10-09 16:51:11 +0800

    fix defect: util/sed-regex-inplace.sh: x-util-is-compatible-i-bsd: command not found

--- functions/util/sed-regex-inplace.sh
+++ functions/util/sed-regex-inplace.sh
@@ -11,13 +11,13 @@
 #?   @sed-regex-inplace [SED_OPTIONS]
 #?
 function sed-regex-inplace () {
-    if x-util-is-compatible-sed-r && x-util-is-compatible-i-bsd; then
+    if x-util-is-compatible-sed-r && x-util-is-compatible-sed-i-bsd; then
         sed -r -i '' "$@"
-    elif x-util-is-compatible-sed-r && x-util-is-compatible-i-gnu; then
+    elif x-util-is-compatible-sed-r && x-util-is-compatible-sed-i-gnu; then
         sed -r -i "$@"
-    elif x-util-is-compatible-sed-E && x-util-is-compatible-i-bsd; then
+    elif x-util-is-compatible-sed-E && x-util-is-compatible-sed-i-bsd; then
         sed -E -i '' "$@"
-    elif x-util-is-compatible-sed-E && x-util-is-compatible-i-gnu; then
+    elif x-util-is-compatible-sed-E && x-util-is-compatible-sed-i-gnu; then
         sed -E -i "$@"
     else
         return 255
