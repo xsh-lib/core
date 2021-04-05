a3a966e71770497b379aa2c41ea3ab64ebdfa14b
Alex <alexzhangs@gmail.com>
2021-04-06 01:05:54 +0800

    export vars in __init__.sh and document

--- functions/date/__init__.sh
+++ functions/date/__init__.sh
@@ -3,41 +3,47 @@
 #?
 #? This file is sourced while importing any function utility, right before the
 #? function utility was sourced.
+#?
+#? All variables except those of Array should be exported in order to be
+#? available for the sub-processes.
+#?
+#? The variables of Array can't be exported to the sub-processes due to the
+#? limitation of Bash.
 #? -----------------------------------------------------------------------------
 
 
 # mmddHHMMyyyy.ss
-XSH_X_DATE__POSIX_FMT="+%m%d%H%M%Y.%S"
+export XSH_X_DATE__POSIX_FMT="+%m%d%H%M%Y.%S"
 
 # yyyy-mm-dd
-XSH_X_DATE__DATE_FMT="+%Y-%m-%d"
+export XSH_X_DATE__DATE_FMT="+%Y-%m-%d"
 
 # HH:MM:SS
-XSH_X_DATE__TIME_FMT="+%H:%M:%S"
+export XSH_X_DATE__TIME_FMT="+%H:%M:%S"
 
 # yyyy-mm-dd HH:MM:SS
-XSH_X_DATE__DATETIME_FMT="+%Y-%m-%d %H:%M:%S"
+export XSH_X_DATE__DATETIME_FMT="+%Y-%m-%d %H:%M:%S"
 
 # 1..7
-XSH_X_DATE__WEEK_NUMBER_OF_WEEK_FMT="+%u"
+export XSH_X_DATE__WEEK_NUMBER_OF_WEEK_FMT="+%u"
 
 # 1..53
-XSH_X_DATE__WEEK_NUMBER_OF_YEAR_FMT="+%V"
+export XSH_X_DATE__WEEK_NUMBER_OF_YEAR_FMT="+%V"
 
 # 2019
-XSH_X_DATE__YEAR_FMT="+%Y"
+export XSH_X_DATE__YEAR_FMT="+%Y"
 
 # 01-12
-XSH_X_DATE__MONTH_FMT="+%m"
+export XSH_X_DATE__MONTH_FMT="+%m"
 
 # 01-31
-XSH_X_DATE__DAY_FMT="+%d"
+export XSH_X_DATE__DAY_FMT="+%d"
 
 # 00-23
-XSH_X_DATE__HOUR_FMT="+%H"
+export XSH_X_DATE__HOUR_FMT="+%H"
 
 # 00-59
-XSH_X_DATE__MINUTE_FMT="+%M"
+export XSH_X_DATE__MINUTE_FMT="+%M"
 
 # 00-60
-XSH_X_DATE__SECOND_FMT="+%S"
+export XSH_X_DATE__SECOND_FMT="+%S"
--- functions/util/__init__.sh
+++ functions/util/__init__.sh
@@ -3,6 +3,12 @@
 #?
 #? This file is sourced while importing any function utility, right before the
 #? function utility was sourced.
+#?
+#? All variables except those of Array should be exported in order to be
+#? available for the sub-processes.
+#?
+#? The variables of Array can't be exported to the sub-processes due to the
+#? limitation of Bash.
 #? -----------------------------------------------------------------------------
 
 
